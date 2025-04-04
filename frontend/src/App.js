0f5b8d4c562d817f8d0204853decb06735d33cf4
Baraka Mukungu <bethelybaraka@gmail.com>
2025-04-04 11:23:55 +0300

    Fixed routing for GitHub Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import Home from "./components/Home";
 import Header from './components/Header';
 import Booking from "./components/Booking";
 import About from './components/About';
